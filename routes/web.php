ae88e5dc0cb0eaf976a3c418b6b4b5bb73f8f2c6
shuhei <shuyume0522@gmail.com>
2019-03-25 16:23:33 +0900

    kadai_11

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['prefix' => 'admin'],function(){
+  Route::get('news/create','Admin\NewsController@add');
+});
+//自コメント)  Route::get('news/create','Admin/NewsController@add');に['prefix' => 'admin']という背定を適用させている。
+/*
+Route::get('admin/news/create','Admin\AAAController@bbb');
+*/
+Route::get('XXX','AAAController@bbb');
+//書き方参照　https://qiita.com/michiomochi@github/items/de19c560bc1dc19d698c
+
+Route::group(['prefix' => 'admin'], function(){
+  Route::get('admin/profile/create','Admin\ProfileController@add');
+  Route::get('admin/profile/edit','Admin\ProfileController@edit');
+});
+// /はディレクトリ階層
+// \は
